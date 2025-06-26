9b48a4e826d1e30a9ea73ea916b7703253ec4b62
Lau-Rom <laurajazminromero0@gmail.com>
2025-06-25 19:35:23 -0600

    Del archivo home_semillero.php se dividio en dos partes aqui quedo solo HTML

--- semillero/home_semillero.php
+++ semillero/home_semillero.php
@@ -1,51 +1,10 @@
+<!--Aqui se agrego un archivo PHP que funciona como la logica-->
 <?php
-
-session_start();
-
-	//Comprobamos si esta definida la sesión 'tiempo'.
-if(isset($_SESSION['tiempo']) ) {
-
-    //Tiempo en segundos para dar vida a la sesión.
-    $inactivo = 1000;//20min en este caso.
-
-    //Calculamos tiempo de vida inactivo.
-    $vida_session = time() - $_SESSION['tiempo'];
-
-        //Compraración para redirigir página, si la vida de sesión sea mayor a el tiempo insertado en inactivo.
-    if($vida_session > $inactivo)
-    {
-            //Removemos sesión.
-    	session_unset();
-            //Destruimos sesión.
-    	session_destroy();              
-            //Redirigimos pagina.
-    	header("Location: index.html");
-
-    	exit();
-    }
-} else {
-    //Activamos sesion tiempo.
-	$_SESSION['tiempo'] = time();
-}
-
-
-
-
-
-
-if(!isset($_SESSION["usuario"])){
-
-	header("location:../index.html");
-}
-$nombre_de_agrupacion = $_SESSION['nombre_de_agrupacion'];
-$tipo_agrupacion = $_SESSION["tipo_agrupacion"];
-$estado = $_SESSION['estado'];
-$estatus = $_SESSION['estatus'];
-$clave = $_SESSION["clave"]
-
+require("valida_home_semillero.php");
 ?>
+<!--Este es un archivo HTML que contiene la presentación-->
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
